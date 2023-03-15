80327667d86094207993d8f0911a59d8a7312caf
fayou bu <fayou@qq.com>
2023-03-15 22:20:07 +0800

    about page

--- main.go
+++ main.go
@@ -6,7 +6,14 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, here is hello-go</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, here is hello-go</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "This repository for recording learnning "+
+			"<a href=\"mailto:albert@example.com\">albert@example.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>not found</h1>")
+	}
 }
 
 func main() {
